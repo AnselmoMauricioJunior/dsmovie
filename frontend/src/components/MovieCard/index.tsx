240b01d76104052d2da5a6eb90a870ae643fb147
AnselmoMauricioJunior <anselmo.mauricio.jr@gmail.com>
2022-01-19 00:55:09 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,14 +1,12 @@
 import MovieScore from "../MovieScore";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Movie } from "types/movie";
 
-function MovieCard() {
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type Props = {
+    movie: Movie;
+}
+
+function MovieCard({ movie }: Props) {    
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
@@ -16,9 +14,9 @@ function MovieCard() {
                 <h3>{movie.title}</h3>
                 <MovieScore />
                 <Link to={`/form/${movie.id}`}>
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
-                
+
             </div>
         </div>
     );
