ca990215eee8cc859f8c56531f988532b36152ac
AnselmoMauricioJunior <anselmo.mauricio.jr@gmail.com>
2022-01-19 00:25:36 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
-import MovieCard from "../../components/MovieCard"
-import Pagination from "../../components/Pagination"
+import axios from "axios";
+import MovieCard from "../../components/MovieCard";
+import Pagination from "../../components/Pagination";
+import { BASE_URL } from "utils/request";
+
 
 function Listing() {
+
+
+    axios.get(`${BASE_URL}/movies?size12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
 
     return (
         <>
